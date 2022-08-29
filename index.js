ab8d7e1b14926f4fa5e68b56620118d65fc3b098
“lilongthinker” <lilongthinker@163.com>
2022-08-30 04:32:16 +0800

    add exec systemcall code

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ const { clear, debug } = flags;
 		// call function ls the  cli.input[1] type
 		// subcommand = cli.input[0]
 		//resourceType = cli.input[1]
-		
+		var exec = require('child_process').exec;
+		var cmd = 'pwd';
+
+		exec(cmd, function (error, stdout, stderr) {
+			// 获取命令执行的输出
+			//log(error);
+			log(stdout);
+			//log(stderr);
+		});
 
 	}	
 })();
